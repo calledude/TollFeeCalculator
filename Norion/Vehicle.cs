202554eaf7647c8330cc98c1bbc760a70acac2dc
calledude <22471295+calledude@users.noreply.github.com>
2024-12-12 22:48:52 +0100

    Namespace touchup/use filescoped namespaces

--- Norion/Vehicle.cs
+++ Norion/Vehicle.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace TollFeeCalculator;
 
-namespace TollFeeCalculator
+public interface Vehicle
 {
-    public interface Vehicle
-    {
-        String GetVehicleType();
-    }
+    String GetVehicleType();
 }
