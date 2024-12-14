a748fd17b9a798cdf06829fb91879316b7626baf
calledude <22471295+calledude@users.noreply.github.com>
2024-12-14 12:23:24 +0100

    Split toll free date checking into separate if-statements

--- Norion/TollCalculator.cs
+++ Norion/TollCalculator.cs
@@ -1,5 +1,6 @@
 ﻿using Norion;
 using Norion.Vehicles;
+using System.Diagnostics.CodeAnalysis;
 
 namespace TollFeeCalculator;
 
@@ -73,24 +74,35 @@ public static class TollCalculator
 
     private static bool IsTollFreeDate(DateTime date)
     {
-        var year = date.Year;
         var month = date.Month;
         var day = date.Day;
 
         if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
             return true;
 
-        if (month == 1 && day == 1 ||
-            month == 3 && (day == 28 || day == 29) ||
-            month == 4 && (day == 1 || day == 30) ||
-            month == 5 && (day == 1 || day == 8 || day == 9) ||
-            month == 6 && (day == 5 || day == 6 || day == 21) ||
-            month == 7 ||
-            month == 11 && day == 1 ||
-            month == 12 && (day == 24 || day == 25 || day == 26 || day == 31))
-        {
+        if (month == 1 && day == 1)
             return true;
-        }
+
+        if (month == 3 && day == 28 || day == 29)
+            return true;
+
+        if (month == 4 && day == 1 || day == 30)
+            return true;
+
+        if (month == 5 && day == 1 || day == 8 || day == 9)
+            return true;
+
+        if (month == 6 && day == 5 || day == 6 || day == 21)
+            return true;
+
+        if (month == 7)
+            return true;
+
+        if (month == 11 && day == 1)
+            return true;
+
+        if (month == 12 && day == 24 || day == 25 || day == 26 || day == 31)
+            return true;
 
         return false;
     }
