fe4bea83286701ae2d57ed61c9d6c0ac24ff914c
calledude <22471295+calledude@users.noreply.github.com>
2024-12-17 19:56:41 +0100

    Add rudimentary Program file with Main()

---
+++ Norion/Program.cs
@@ -0,0 +1,16 @@
+﻿using Norion.Vehicles;
+using TollFeeCalculator;
+
+namespace Norion;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var offset = DateTimeOffset.Now.Offset;
+        var date = new DateTimeOffset(2024, 12, 17, 08, 00, 00, offset);
+
+        var fee = TollCalculator.GetTollFee(new Car(), [date]);
+        Console.WriteLine(fee);
+    }
+}
